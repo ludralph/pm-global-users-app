026020fc449664bd101f19d757ef6cfe3d27a6da
Raphael Etim <raphaelumoh@gmail.com>
2020-09-03 10:02:18 +0100

    add middlewares

--- src/app.js
+++ src/app.js
@@ -1,22 +1,36 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import compress from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
+import userRoutes from './routes/user.routes';
+import errorHandler from './utils/error';
 
 dotenv.config();
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+const {
+  bodyParserHandler,
+  globalErrorHandler,
+  fourOhFourHandler,
+  fourOhFiveHandler,
+} = errorHandler;
+
+app.use(compress());
+app.use(helmet());
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParserHandler);
+
+app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
-  res.send({message: 'Welcome to User Restful API' });
+  res.send({ message: 'Welcome to User Restful API' });
 });
+app.get('*', fourOhFourHandler); // catch-all for 404 "Not Found" errors
+app.all('*', fourOhFiveHandler); // catch-all for 405 "Method Not Allowed" errors
+app.use(globalErrorHandler);
 export default app;
